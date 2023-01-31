69cb3b744a0a692fd20e0835750daedcb647632e
Chris ✨ <15004900+nowchris@users.noreply.github.com>
2023-01-31 13:10:37 -0500

    User Creation Migration params

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('role_as')->default('0');
             $table->string('name', 255); // 255 character limit
             $table->string('email', 255)->unique(); // 255 character limit
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->string('employee_id', 20); // 20 character limit for employee ID
             $table->rememberToken();
             $table->timestamps();
