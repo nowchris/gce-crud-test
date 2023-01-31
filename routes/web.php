74ca8ea8d1a7d6af0fe0c5f8c400268c5368aefc
Chris ✨ <15004900+nowchris@users.noreply.github.com>
2023-01-31 13:11:00 -0500

    Route init

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('users', UserController::class);
+
+
+//Middleware to authenticate a user
+
+// Route::middleware(['auth','isAdmin'])->group(function() {
+    // Route::resource('users', UserController::class);
+// });
