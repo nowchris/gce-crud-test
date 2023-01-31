8c64851ebbbfc38ddcbff7a0262f70792f8faa2e
Chris ✨ <15004900+nowchris@users.noreply.github.com>
2023-01-31 13:10:16 -0500

    User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,25 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'employee_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
